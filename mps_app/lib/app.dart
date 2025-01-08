908ab11718f82ec4ef5b164899954fcfe1b6ec75
Emerson Viçosa <emersonvicosa@gmail.com>
2025-01-07 23:03:03 -0300

    video_03

--- mps_app/lib/app.dart
+++ mps_app/lib/app.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mps_app/features/splash/splash_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Coe gurizada')),
+    return MaterialApp(
+      home: SplashPage(
       ),
     );
   }
